93da8f96eb9657248e095c94c1f07d308466ff83
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-04-07 17:32:48 +0530

    commit

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,15 +1,17 @@
-import axios from 'axios'
+import axios from 'axios';
+import {FaHeart,FaRegHeart} from 'react-icons/fa'
 import React, { useEffect, useState } from 'react'
 
 const Row = ({title,fetchURL}) => {
 
     const[movies,setMovies]=useState([])
+    const[like,setLike]=useState(false)
 
     useEffect(()=>{
         axios.get(fetchURL).then((response)=>{
             setMovies(response.data.results)
         })
-    },[])
+    },[fetchURL])
 
     
   return (
@@ -23,6 +25,14 @@ const Row = ({title,fetchURL}) => {
                     className='w-full h-auto block' 
                     src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`}
                      alt={item?.title}/>
+                    <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+                        <p className='white-space-normal text-xl md:text-sm font-bold flex justify-center items-center h-full text-center'>
+                        {item?.title}
+                        </p>
+                        <p>
+                            {like ? <FaHeart className='absolute top-4 left-4 text-gray-300'/> : <FaRegHeart className='absolute top-4 left-4 text-gray-300'/>}
+                        </p>
+                    </div>
                 </div>
             )}
         </div>
