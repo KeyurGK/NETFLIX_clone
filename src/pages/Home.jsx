26af874c1865a9862220f35939d1065d6d0694b3
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-04-07 12:33:37 +0530

    api calls in the row component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Main from '../components/Main'
+import Row from '../components/Row'
+import requests from '../Requests'
 
 const Home = () => {
   return (
     <div>
       <Main/>
+      <Row title='Popular' fetchURL={requests.requestPopular}/>
+      <Row title='Upcoming' fetchURL={requests.requestUpcoming}/>
+      <Row title='Top Rated' fetchURL={requests.requestTopRated}/>
+      <Row title='Horror' fetchURL={requests.requestHorror}/>
+      <Row title='Trending' fetchURL={requests.requestTrending}/>
     </div>
   )
 }
