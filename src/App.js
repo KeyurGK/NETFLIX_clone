1c67867dae4949889a19bc713a37c18a42181728
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-04-09 16:04:34 +0530

    comitting login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
+import LogIn from './pages/LogIn';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
 import { AuthContextProvider } from "./context/AuthContext";
 
 
@@ -11,6 +14,9 @@ function App() {
     <Nav/> 
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/login' element={<LogIn/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/account' element={<Account/>}/>
     </Routes>
    </AuthContextProvider>
     </div>
