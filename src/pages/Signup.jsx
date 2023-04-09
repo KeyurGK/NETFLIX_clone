5c71db34b8a4cb166293b293c6eafe8dd4777a4d
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-04-09 16:18:43 +0530

    new user sign up commit

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Signup = () => {
+
+  const [email,setEmail]=useState('')
+  const[password,setPassword]=useState('')
+  const {user,signUp} = UserAuth()
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try{
+      await signUp(email,password)
+    }
+    catch(error)
+    {
+      console.log(error)
+    }
+  } 
   return (
     <div>
       <div className='w-full h-screen'>
@@ -10,9 +25,9 @@ const Signup = () => {
           <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white border border-white'>
               <div className='max-w-[320px] mx-auto py-16'>
               <h1 className=' text-3xl font-bold'>Sign Up</h1>
-                <form className='w-full flex flex-col py-4'>
-                  <input className='p-3 my-2 bg-gray-700' type='email' placeholder='Email'/>
-                  <input className='p-3 my-2 bg-gray-700' type='password' placeholder='Password'/>
+                <form className='w-full flex flex-col py-4' onSubmit={handleSubmit}>
+                  <input className='p-3 my-2 bg-gray-700' type='email' placeholder='Email' onChange={(e)=>setEmail(e.target.value)}/>
+                  <input className='p-3 my-2 bg-gray-700' type='password' placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
                   <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
                   <div className='flex justify-between items-center text-gray-600'>
                     <p><input className='mr-2' type='checkbox'/>Remember</p>
