7c7a58a0be56e3b3e6cde5d5e8873855d7a1278d
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-07-08 20:37:32 +0530

    structured and styled accounts page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { UserAuth } from "../context/AuthContext";
 
 const Account = () => {
+  const { user } = UserAuth();
   return (
-    <div>Account</div>
-  )
-}
+    <div className="flex items-center justify-center w-full h-full absolute top-0 left-0">
+      <div className="max-w-[300px] h-[300px] bg-black/75 text-white border border-white py-16 px-8 my-16">
+        <h3 className="underline">Username</h3>
+        <p className="mb-4 pb-2">{user?.email}</p>
+        <h3 className="underline">Account Created</h3>
+        <p className="mb-4 pb-2">{user?.metadata?.creationTime}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Account
+export default Account;
